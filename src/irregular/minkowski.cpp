a55e950576c29ad62ed4eb2dbb2220c218b85f5d
Michelle <mmmaufrand@gmail.com>
2024-10-16 12:14:08 +0200

    modifs reverse poly + code layout + hpp filled + filename changed

--- src/irregular/minkowski.cpp
+++ src/irregular/minkowski.cpp
@@ -9,41 +9,37 @@ typedef CGAL::Polygon_with_holes_2<K> Polygon_with_holes_2;
 typedef Polygon_2::Vertex_iterator VertexIterator;
 typedef Polygon_2::Edge_const_iterator EdgeIterator;
 
-namespace packingsolver
+using namespace packingsolver;
+using namespace packingsolver::irregular;
+
+Point_2 irregular::get_point(
+    ShapeElement shape_el) 
 {
-namespace irregular
-{ 
-Point_2 get_point(ShapeElement shape_el) {
     return Point_2(shape_el.start.x,shape_el.start.y);
 }
 
-Polygon_2 get_poly(Shape s) {
-    // dabord il faut des points avec get_point
+//from shape to polygon2
+Polygon_2 irregular::get_poly(
+    Shape s) 
+{
+    // need points of the Shape
     int n = s.elements.size();
     Point_2* points = new Point_2[n];
     int i = 0;
-    for (ShapeElement element = s.elements.begin(); element!=s.elements.end() ; element++, i++)
+    for (int j = 0; j<=s.elements.size() ; j++, i++)
     {
-        points[i] = get_point(element);
+        points[i] = get_point(s.elements[j]);
     }
     // we have all points
     Polygon_2 pgn(points, points+n);
     return pgn;
 }
 
-Polygon_with_holes_2 NFP( Shape shapeFixed, Shape shapeMobile){
+Polygon_with_holes_2 irregular::NFP( 
+    Shape shapeFixed, Shape shapeMobile)
+{
     Polygon_2 polyFixed = get_poly(shapeFixed);
     Polygon_2 polyMobile = get_poly(shapeMobile);
+    polyMobile.reverse_orientation();
     return minkowski_sum_by_full_convolution_2(polyFixed, polyMobile);
-}
-
-
-
-
-
-int main()
-{    
-}
-
-}
 }
---
+++ src/irregular/minkowski.hpp
@@ -0,0 +1,24 @@
+#include "packingsolver/irregular/instance.hpp"
+#include <CGAL/minkowski_sum_2.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Point_2<K> Point_2;
+typedef CGAL::Polygon_2<K> Polygon_2;
+typedef CGAL::Polygon_with_holes_2<K> Polygon_with_holes_2;
+
+namespace packingsolver
+{
+
+namespace irregular
+{
+
+Point_2 get_point(
+    ShapeElement shape_el);
+
+Polygon_2 get_poly(
+    Shape s);
+
+Polygon_with_holes_2 NFP( 
+    Shape shapeFixed, Shape shapeMobile);
+}
+}
