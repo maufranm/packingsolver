35d0110c5c1dc8b7d9afed2e5a06646902e33241
Michelle <mmmaufrand@gmail.com>
2024-10-18 11:48:31 +0200

    init tests minkowski

---
+++ test/irregular/minkowski_test.cpp
@@ -0,0 +1,17 @@
+#include "irregular/minkowski.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace packingsolver;
+using namespace packingsolver::irregular;
+
+namespace
+{
+    // get_poly
+    // est ce que je retourne un poly ? 
+    // est ce que ses points sont bien ceux que je veux ?
+    
+    // NFP
+    // est ce que le nfp retourné est un poly ?
+    // est ce que ce sont les bons points qui sont retournés ?
+}
